325341ea768c17ae04fd89250ea5f3e459de7426
Ipstenu <Ipstenu@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-06-03 13:29:22 +0000

    Troubleshooting bbPress
    git-svn-id: https://plugins.svn.wordpress.org/basic-comment-quicktags/tags/1.4@552492 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ quicktags.js
@@ -0,0 +1,17 @@
+// (function ($) {
+//   $(document).ready(function($) {
+    quicktags({
+      id: "comment",
+      buttons: "link,em,strong"
+    });
+    quicktags({
+      id: "bbp_reply_content",
+      buttons: "link,em,strong"
+    });
+    quicktags({
+      id: "bbp_topic_content",
+      buttons: "link,em,strong"
+    });
+    QTags.addButton('quote','quote','<blockquote>','</blockquote>','quote');
+//   });
+// })(jQuery);
